6b4e0195c9f1ad2af25d0030e7548b4246e08517
EmedeWal <emewillemdewal@gmail.com>
2025-03-26 22:01:22 +0100

    Credits implemented

--- Assets/Credits/CreditController.cs
+++ Assets/Credits/CreditController.cs
@@ -1,9 +1,34 @@
-namespace Context
+namespace Context.UI
 {
     using UnityEngine;
+    using UnityEngine.Video;
 
+    [RequireComponent(typeof(VideoPlayer))]
     public class CreditController : MonoBehaviour
     {
+        private InputActions _actions;
+        private VideoPlayer _player;
 
+        private void Start()
+        {
+            _player = GetComponent<VideoPlayer>();
+            
+            _player.Play();
+            _player.loopPointReached += OnVideoFinished;
+
+            _actions = ApplicationManager.Instance.InputManager.Actions;
+        }
+
+        private void Update()
+        {
+            var menu = _actions.Menu;
+            if (menu.Continue.WasPressedThisFrame())
+                OnVideoFinished(_player);
+        }
+
+        private void OnVideoFinished(VideoPlayer videoPlayer)
+        {
+            ApplicationManager.Instance.SceneLoader.TransitionToScene(0);
+        }
     }
 }
