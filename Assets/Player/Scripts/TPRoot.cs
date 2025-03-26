29123dfc0b11c832c8a859f73692d34a4464f6af
EmedeWal <emewillemdewal@gmail.com>
2025-03-26 13:41:15 +0100

    Work on different footsteps (rocks/sand) based on groundtype

--- Assets/Player/Scripts/TPRoot.cs
+++ Assets/Player/Scripts/TPRoot.cs
@@ -4,13 +4,23 @@ namespace Context.ThirdPersonController
 
     public class TPRoot : MonoBehaviour
     {
+        [Header("REFERENCES")]
+
+        [Space]
+        [Header("Particles")]
         [SerializeField] private ParticleSystem _smallDust;
         [SerializeField] private ParticleSystem _bigDust;
 
+        [Header("SETTINGS")]
+
+        [Space]
+        [Header("Land Settings")]
+        [SerializeField] private float _minImpactMagnitude = 10f;
+
         private Transform _followTarget;
         private Transform _transform;
-        
         private TPAnimator _animator;
+        private GroundType _groundType;
 
         public void Init(Transform followTarget)
         {
@@ -33,21 +43,29 @@ namespace Context.ThirdPersonController
             TPAnimator.Footstep -= TPRoot_Footstep;
         }
 
-        public void LateTick(float deltaTime, bool isMoving)
+        public void LateTick(GroundType groundType, float deltaTime, bool isMoving)
         {
             _transform.SetPositionAndRotation(_followTarget.position, _followTarget.rotation);
 
             _animator.UpdateAnimations(deltaTime, isMoving);
+
+            _groundType = groundType;
         }
 
         private void TPRoot_Footstep()
         {
-            _smallDust.Play();
+            if (_groundType is GroundType.Sand)
+            {
+                _smallDust.Play();
+            }
         }
 
-        private void TPRoot_Landed()
+        private void TPRoot_Landed(Vector3 velocity)
         {
-            _bigDust.Play();
+            if (_groundType is GroundType.Sand && velocity.magnitude > _minImpactMagnitude)
+            {
+                _bigDust.Play();
+            }
         }
     }
 }
