f3e405253b8c2880ddfac5977146cafdae915ba4
bombarman <armanaryob@hotmail.com>
2025-03-27 12:51:33 +0100

    cutscene golem light fix

--- Assets/Scripts/Utils/MaterialSwapper.cs
+++ Assets/Scripts/Utils/MaterialSwapper.cs
@@ -1,43 +1,79 @@
+using UnityEngine;
+
 namespace Context
 {
-    using UnityEngine;
-
     public class MaterialSwapper : MonoBehaviour
     {
         [Header("REFERENCES")]
-
-        [Space]
-        [Header("Activation")]
         [SerializeField] private Material _startMaterial;
 
-        [Space]
         [Header("Materials")]
-        [SerializeField] private Material _defaultMaterial;
-        [SerializeField] private Material _litMaterial;
+        [SerializeField] private Material _defaultMaterial; // Dark Material
+        [SerializeField] private Material _litMaterial; // Light Material
 
-        [Space]
         [Header("Hierarchy")]
-        [SerializeField] private SkinnedMeshRenderer _bodyMeshRenderer;
         [SerializeField] private MeshRenderer[] _meshRenderers;
+
+        [Header("Settings")]
+        [SerializeField] private bool _shouldSwap = true; // Control swapping per object
+
+        private bool _isLit; // Tracks current material state
 
         private void Start()
         {
             SetMaterials(_startMaterial);
+
+            if (_shouldSwap) // Only start swapping if enabled
+                StartSwapping();
         }
 
-        public void SetDefaultMaterials() =>
-            SetMaterials(_defaultMaterial);
+        public void StartSwapping()
+        {
+            if (!_shouldSwap) return; // Prevents starting if disabled
+            InvokeRepeating(nameof(SwapMaterials), 1f, 1f);
+        }
 
-        public void SetLitMaterials() =>
-            SetMaterials(_litMaterial);
+        public void StopSwapping()
+        {
+            CancelInvoke(nameof(SwapMaterials));
+            SetMaterials(_defaultMaterial); // Reset to dark material
+            _isLit = false; // Ensure state is reset
+        }
+
+        private void SwapMaterials()
+        {
+            if (!_shouldSwap) return; // If swapping is disabled, do nothing
+            _isLit = !_isLit;
+            SetMaterials(_isLit ? _litMaterial : _defaultMaterial);
+        }
 
         private void SetMaterials(Material material)
         {
-            if (_bodyMeshRenderer != null)
-                _bodyMeshRenderer.material = material;
-
             foreach (var renderer in _meshRenderers)
                 renderer.material = material;
+        }
+
+        public void ToggleSwapping(bool enable)
+        {
+            _shouldSwap = enable;
+
+            if (_shouldSwap)
+                StartSwapping();
+            else
+                StopSwapping();
+        }
+
+        // New Functions to Manually Set Materials
+        public void SetDarkMaterial()
+        {
+            SetMaterials(_defaultMaterial); // Set to dark material
+            _isLit = false; // Ensure the state reflects dark material
+        }
+
+        public void SetLightMaterial()
+        {
+            SetMaterials(_litMaterial); // Set to light material
+            _isLit = true; // Ensure the state reflects light material
         }
     }
 }
